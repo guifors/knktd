c2727f3a2ec393d6b9c8f8f212e953314493c0b6
Guillermo Forteza <guifors@gmail.com>
2020-06-17 11:35:12 +0100

    fixed proposals bug

--- db/migrate/20200616094057_add_fields_to_startups.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToStartups < ActiveRecord::Migration[5.2]
-  def change
-    add_column :proposals, :usp, :string
-    add_column :proposals, :poc_description, :string
-    add_column :proposals, :poc_examples, :string
-    add_column :proposals, :match_criteria, :boolean, default: false
-  end
-end
