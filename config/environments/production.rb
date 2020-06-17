bcf6f25f09020442dd84b23e579d4daf9fa5b699
Guillermo Forteza <guifors@gmail.com>
2020-06-17 11:52:14 +0100

    added production config emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://www.konektd.io" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
